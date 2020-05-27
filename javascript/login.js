7a632ded672143bb9153c3c88876765d7bebaa94
oscar lopez <oscarlopez88991@gmail.com>
2020-05-27 15:42:40 -0500

    setting logo

---
+++ javascript/login.js
@@ -0,0 +1,14 @@
+let user = "oscar"
+let password = "123"
+
+function recibirData(keyboarUser, keyboarPassword){
+    if(keyboarUser === user && keyboarPassword){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function login(){
+    
+}
