c8ccd25e7880b14961df4c1d67cdc1e99dcd5a8f
oscar lopez <oscarlopez88991@gmail.com>
2020-05-27 12:28:16 -0500

    load global

---
+++ javascript/home.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu");
+const bugerButton = document-querySelector("#bugerMenu");
+
+bugerButton.addEventListener('click', hidenShow)
+    function hidenShow(){
+        if(menu.classList.contains('is-active')){
+            menu.classList.remove('is-active');
+        }else{
+            menu.classList.add('is-active');
+        }
+    }
+
